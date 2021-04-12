38e2b7acca5cfef65e9ffd97ed184edd8b933490
Jon Exume <jonexume@me.com>
2021-04-12 00:34:28 -0400

    New commit for app
    New app

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Jon Exume on 4/11/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
